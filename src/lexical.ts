56aa95e7d95189556407a6c284879e057bb07edd
LayMui <laymui.toh@yara.com>
2021-12-16 16:22:01 +0800

    Add arrow function to capture the this keyword

--- src/lexical.ts
+++ src/lexical.ts
@@ -4,13 +4,15 @@ class Person {
     this._age = _age
   }
 
-  // growOld = () => {
-  //   this._age++
-  // }
 
-  growOld() {
+  // Arrow function in JS captured ths this from the surrounding context
+  growOld = () => {
     this._age++
   }
+
+  // growOld() {
+  //   this._age++
+  // }
 
   age() {
     return this._age
@@ -23,4 +25,8 @@ for(let i=0; i<20;i++) {
  person.growOld();
 }
 
+
+
 console.log('age: ', person.age());
+
+
